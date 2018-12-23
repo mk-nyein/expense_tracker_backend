a2f0f32a8b6df3bd93b6deca4c644b6a0b17a2a9
may khine nyein <mknyein@zwenex.com>
2018-12-23 13:49:46 +0630

    fix date_time to datetime in migration files

--- db/migrate/20181223071748_create_incomes.rb
+++ db/migrate/20181223071748_create_incomes.rb
@@ -2,7 +2,7 @@ class CreateIncomes < ActiveRecord::Migration[5.2]
   def change
     create_table :incomes do |t|
       t.string :description
-      t.date_time :date
+      t.datetime :date
       t.integer :amount
       t.integer :category_id
 
--- db/migrate/20181223071819_create_expeses.rb
+++ db/migrate/20181223071819_create_expeses.rb
@@ -2,7 +2,7 @@ class CreateExpeses < ActiveRecord::Migration[5.2]
   def change
     create_table :expeses do |t|
       t.string :description
-      t.date_time :date
+      t.datetime :date
       t.integer :amount
       t.integer :category_id
 
