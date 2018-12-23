8d4a82ba2bc5ddc51f4080648209fc3483708915
may khine nyein <mknyein@zwenex.com>
2018-12-23 14:04:48 +0630

    add actions in version1 controller

--- app/controllers/api/version1_controller.rb
+++ app/controllers/api/version1_controller.rb
@@ -1,2 +1,54 @@
 class Api::Version1Controller < ApplicationController
+
+  def dashboard
+    
+  end
+  
+  def income_chart
+    
+  end
+  def expense_chart
+  
+  end
+  
+  def monthly_income_chart
+
+  end
+  def monthly_expense_chart
+    
+  end
+  
+  
+  
+  def add_income
+    
+  end
+  def update_income
+    
+  end
+  def delete_income
+    
+  end
+  def index_income
+    
+  end
+
+  def add_expense
+    
+  end
+  def update_expense
+    
+  end
+  def delete_expense
+    
+  end
+  def index_expense
+    
+  end
+
+
+  
+  
+  
+  
 end
